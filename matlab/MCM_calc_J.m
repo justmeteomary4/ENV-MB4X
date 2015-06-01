3f4a4018bea4a97318d5f18ea13f577c247927a3
maria.zamyatina <maria.zamyatina.int@gmail.com>
2015-06-01 15:33:12 +0100

    Minor changes

--- matlab/MCM_calc_J.m
+++ matlab/MCM_calc_J.m
@@ -21,8 +21,8 @@ for n = 1:length(td1)
 end
 %% Read NOAA solar calculations output
 % (sun declination angle, hour angle, solar zenith angle)
-lat = 45; lon = -30; tzone = -1; date = '01.07.2015';
-fid2 = fopen([indir,'/','NOAA_dec_hour_zenith_angle_min.dat'],'r');
+lat = 45; lon = -30; tzone = -1; date = '01.07.2015'; step = 'min';
+fid2 = fopen([indir,'/','NOAA_dec_hour_zenith_angle_',step,'_minus.dat'],'r');
 format2 = '%f %f %f';
 rd2 = textscan(fid2,format2,'headerlines',1);
 fclose(fid2);
@@ -46,15 +46,16 @@ for i = 1:length(td1) % MCM parameters loop
         J4(i,j) = exp(-pn(i).*J3(j));
         J5(i,j) = J2(i,j).*J4(i,j);
     end
+    Jmean(i) = nanmean(J5(i,:));
 end
 %% Plot photolysis rate coefficient timeseries
 for in = 1:length(name)
     figure;
     plot(J5(in,:));
-    imgname = strcat(outdir,name{in},'.png');
+    imgname = strcat(outdir,step,'_',name{in},'.png');
     title(strcat(name{in},' photolysis rate coefficient'));
-    xlabel('Time, min'); xlim([0 length(td2)]);
-    ylabel('J, 1/s');
+    xlabel(strcat('Time, ',step)); xlim([0 length(td2)]);
+    ylabel(strcat('J, 1/',step));
     set(gcf,'visible','off')
     print(gcf,'-dpng','-r300',imgname);
 end
