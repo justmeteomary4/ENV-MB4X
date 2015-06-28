82bc7614ebf9431279d8155a732caab681d0ee1b
maria.zamyatina <maria.zamyatina.int@gmail.com>
2015-06-28 23:20:27 +0100

    Calculation of number of carbon bonds from CH4 only

--- matlab/ANsCB_read_flux.m
+++ matlab/ANsCB_read_flux.m
@@ -1,76 +1,99 @@
-% Read and plot resulting mixing ratios from ANsCB model
-%% Read mixing ratios
+% Read and plot resulting 'fluxes' from ANsCB model
+%% Read nett reaction rates (RF % KF : A = B, where RF = KF*A)
 clear; clc;
 indir = '..';
 outdir = 'ANsCB_pics';
-fname = 'flux_0_00';
+fname = 'flux_0_01';
 fnamelong = [indir,'/',fname,'.dat'];
 f = importdata(fnamelong,' ',2);
-td = f.data;
+RF = f.data;
 for i = 1:numel(f.colheaders)
     header{i} = f.colheaders{i}(2:end); % read headers from 2nd letter
 end
-fluxnames = {'KO3' 'KO12' 'KO13' 'KO25' 'KO36' 'KO53' 'KO86' 'KO95' 'KO97' 'KO100' ...
+RFnames = {'KO3' 'KO12' 'KO13' 'KO25' 'KO36' 'KO53' 'KO86' 'KO95' 'KO97' 'KO100' ...
     'KO120' 'KOMCM1' 'KO139A' 'KO139B' 'KO197A' 'KO197B' 'KO209' ...
     'KO40' 'KOMCM2' 'KO211'};
 cvec = {'r' 'r' 'r' 'r' 'r' 'r' 'r' 'r' 'r' 'r' 'r' 'r' 'r' 'r' 'r' 'r' 'r' 'r' 'r' 'r'};
 nrows = 4;
 ncols = 5;
-%% Plot fluxes through each organic reaction
+%% Plot RF of organic reactions
 xend = 3600*24;%length(td1);
 fig=figure;
-for isub = 1:numel(fluxnames)
-    j = find(ismember(header,fluxnames{isub}));
-    subplot(nrows,ncols,isub); plot(td(:,j),'LineWidth',2,'Color',cvec{isub}); title(header{j});
+for isub = 1:numel(RFnames)
+    j = find(ismember(header,RFnames{isub}));
+    subplot(nrows,ncols,isub); plot(RF(:,j),'LineWidth',2,'Color',cvec{isub}); title(header{j});
 end
 faxes = findobj(fig,'Type','Axes');
 for i=1:length(faxes)
     xlabel(faxes(i),'sec','FontSize',9)
-    ylabel(faxes(i),'molecules s-1','FontSize',9) %flux
+    ylabel(faxes(i),'molecules s-1','FontSize',9)
     set(faxes(i),'FontSize',7)
     xlim(faxes(i),[0 xend]);
 end
-imgname = strcat(outdir,'/',fname,'_pic','.png');
+imgname = strcat(outdir,'/',fname,'_RF','.png');
 set(gcf,'visible','off')
-print(gcf,'-dpng','-r300',imgname);
-%% Calculate number of carbon bonds in reactants (CO+CH4)
-cbLOST = 4*td(:,1)+td(:,2);
-% Rectants
-reac{1} = 1*td(:,2); % CO
-reac{2} =  4*td(:,1); % CH4
-reac{3} = 2*td(:,9); % CH2OH
-reac{4} = zeros(length(td),1); % CH2OOH
-reac{5} = 3*td(:,7); % CH3
-reac{6} = 3*td(:,10); % CH3O
-reac{7} = 3*(td(:,6)+td(:,11)+td(:,12)+2*td(:,13)+2*td(:,14)+td(:,19)); % CH3O2
-reac{8} = 3*td(:,4); % CH3OH
-reac{9} = 3*(td(:,5)+td(:,17)); % CH3OOH
-reac{10} = 1*td(:,8); % HCO
-reac{11} = 2*(td(:,3)+td(:,15)+td(:,16)); % HCHO
-reac{12} = 3*(td(:,18)+td(:,20)); % CH3ONO2
-% Products
-prod{1} = 1*(td(:,8)+td(:,16)); % CO
-prod{2} = zeros(length(td),1); % CH4
-prod{3} = 2*(0.85*td(:,4)); % CH2OH
-prod{4} = 2*(0.35*td(:,5)); % CH2OOH
-prod{5} = 3*td(:,1); % CH3
-prod{6} = 3*(0.15*td(:,4)+td(:,11)+td(:,12)+2*td(:,14)+td(:,17)+td(:,20)); % CH3O
-prod{7} = 3*(0.65*td(:,5)+td(:,7)); % CH3O2
-prod{8} = 3*td(:,13); % CH3OH
-prod{9} = 3*0.9*td(:,6); % CH3OOH
-prod{10} = 1*(td(:,3)+td(:,15)); % HCO
-prod{11} = 2*(0.1*td(:,6)+td(:,9)+td(:,10)+td(:,13)+td(:,18)); % HCHO
-prod{12} = 3*td(:,19); % CH3ONO2
-compnames = {'CO' 'CH4' 'CH2OH' 'CH2OOH' 'CH3' 'CH3O' 'CH3O2' 'CH3OH' ...
+% print(gcf,'-dpng','-r300',imgname);
+%% Calculate number of carbon bonds (CB)
+% Initial number of CBs in the system = number of carbons * number density
+ndCO = 2.431*10^(12);
+ndCH4 = 4.375*10^(13); 
+cb_init = 1*ndCO+4*ndCH4;
+% Rectant (left hand side of reaction)
+cbreac_left{1,1} = 1*RF(:,2); % CO
+cbreac_left{1,2} = 4*RF(:,1); % CH4
+cbreac_left{1,3} = 2*RF(:,9); % CH2OH
+cbreac_left{1,4} = zeros(length(RF),1); % CH2OOH
+cbreac_left{1,5} = 3*RF(:,7); % CH3
+cbreac_left{1,6} = 3*RF(:,10); % CH3O
+cbreac_left{1,7} = 3*(RF(:,6)+RF(:,11)+RF(:,12)+2*RF(:,13)+2*RF(:,14)+RF(:,19)); % CH3O2
+cbreac_left{1,8} = 3*RF(:,4); % CH3OH
+cbreac_left{1,9} = 3*(RF(:,5)+RF(:,17)); % CH3OOH
+cbreac_left{1,10} = 1*RF(:,8); % HCO
+cbreac_left{1,11} = 2*(RF(:,3)+RF(:,15)+RF(:,16)); % HCHO
+cbreac_left{1,12} = 3*(RF(:,18)+RF(:,20)); % CH3ONO2
+% Product (right hand side of reaction)
+cbreac_righ{1,1} = 1*(RF(:,8)+RF(:,16)); % CO
+cbreac_righ{1,2} = zeros(length(RF),1); % CH4
+cbreac_righ{1,3} = 2*(0.85*RF(:,4)); % CH2OH
+cbreac_righ{1,4} = 2*(0.35*RF(:,5)); % CH2OOH
+cbreac_righ{1,5} = 3*RF(:,1); % CH3
+cbreac_righ{1,6} = 3*(0.15*RF(:,4)+RF(:,11)+RF(:,12)+2*RF(:,14)+RF(:,17)+RF(:,20)); % CH3O
+cbreac_righ{1,7} = 3*(0.65*RF(:,5)+RF(:,7)); % CH3O2
+cbreac_righ{1,8} = 3*RF(:,13); % CH3OH
+cbreac_righ{1,9} = 3*0.9*RF(:,6); % CH3OOH
+cbreac_righ{1,10} = 1*(RF(:,3)+RF(:,15)); % HCO
+cbreac_righ{1,11} = 2*(0.1*RF(:,6)+RF(:,9)+RF(:,10)+RF(:,13)+RF(:,18)); % HCHO
+cbreac_righ{1,12} = 3*RF(:,19); % CH3ONO2
+cbreac_diff = cellfun(@minus,cbreac_righ,cbreac_left,'UniformOutput',false); % right-left
+% Number of CBs in the system
+cbsys_lost = cbreac_left{1,1}+cbreac_left{1,2}+cbreac_left{1,3}+cbreac_left{1,4}+cbreac_left{1,5}+...
+    cbreac_left{1,6}+cbreac_left{1,7}+cbreac_left{1,8}+cbreac_left{1,9}+cbreac_left{1,10}+...
+    cbreac_left{1,11}+cbreac_left{1,12};
+cbsys_prod = cbreac_righ{1,1}+cbreac_righ{1,2}+cbreac_righ{1,3}+cbreac_righ{1,4}+cbreac_righ{1,5}+...
+    cbreac_righ{1,6}+cbreac_righ{1,7}+cbreac_righ{1,8}+cbreac_righ{1,9}+cbreac_righ{1,10}+...
+    cbreac_righ{1,11}+cbreac_righ{1,12}; % more efficient way?
+cbsys_diff = cbsys_prod-cbsys_lost;
+% Final number of CBs in the system
+cb_end = cbsys_prod(end);
+%% Plot number of carbon bonds reaction-wise (broken/produced)
+spnames = {'CO' 'CH4' 'CH2OH' 'CH2OOH' 'CH3' 'CH3O' 'CH3O2' 'CH3OH' ...
     'CH3OOH' 'HCO' 'HCHO' 'CH3ONO2'};
 step = 'sec';
-for in = 1:length(compnames)
+for in = 1:length(spnames)
     figure;
-    plot(reac{in},'b'); hold on; plot(prod{in},'r'); title(compnames{in}); hold off;
-    imgname = strcat(outdir,'/',fname,'_',compnames{in},'.png');
-    xlabel(strcat('Time, ',step)); xlim([0 length(td)]);
+    plot(cbreac_left{1,in},'b'); hold on; plot(cbreac_righ{1,in},'r'); hold on; plot(cbreac_diff{1,in},'k');
+    title(spnames{in}); hold off;
+    imgname = strcat(outdir,'/',fname,'_',spnames{in},'.png');
+    xlabel(strcat('Time, ',step)); xlim([0 length(RF)]);
     ylabel('number of carbon bonds');
-    legend('lost','produced','Location','southeast');
+    legend('lost','produced','diff','Location','southeast');
     set(gcf,'visible','off')
-    print(gcf,'-dpng','-r300',imgname);
+%     print(gcf,'-dpng','-r300',imgname);
 end
+%% Plot changes
+figure;
+xchange = [-1 86400]; ychange = [cb_init cb_end];
+plot(xchange,ychange,'--ok');
+imgname = strcat(outdir,'/',fname,'_change.png');
+set(gcf,'visible','off')
+print(gcf,'-dpng','-r300',imgname);
