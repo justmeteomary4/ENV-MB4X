6edb70200e7efe6c36a1d896e8223e7888f670a0
maria.zamyatina <maria.zamyatina.int@gmail.com>
2015-07-02 08:00:13 +0100

    Minor changes

--- matlab/ANsCB_read_chem.m
+++ matlab/ANsCB_read_chem.m
@@ -3,14 +3,14 @@
 clear; clc;
 indir = '..';
 outdir = 'ANsCB_pics';
-fname = 'chem_1_01';
+fname = 'chem_1_02';
 fnamelong = [indir,'/',fname,'.dat'];
 f = importdata(fnamelong,' ',2);
 td1 = f.data;
 for i = 1:numel(f.colheaders)
     header{i} = f.colheaders{i}(2:end); % read headers from 2nd letter
 end
-plnames = {'O3' 'O1D' 'OH' 'NO' 'NO2' 'HO2' 'H2O2' 'CO' 'CH4' 'CH3O' 'CH3O2' 'HCHO' 'CH3OH' 'CH3OOH' 'C2H5O' 'CH3NO3' 'C2H5NO3'};
+plnames = {'O3' 'O1D' 'OH' 'NO' 'NO2' 'HO2' 'H2O2' 'CO' 'CH4' 'C2H6' 'CH3O' 'CH3O2' 'C2H5O' 'C2H5O2' 'HCHO' 'CH3NO3' 'C2H5NO3'};
 % cvec   = {'r'      'r'      'r'      'r'       'b'     'r'      'r'        'b'         'b'         'b'           'b'         'b'        'r'      'r'     'b'     'b'          'b' 'b'};
 nrows = 4;
 ncols = 5;
