53cfd0bb7d66051d2b998342f4a4fcf1614738ac
maria.zamyatina <maria.zamyatina.int@gmail.com>
2015-07-13 00:32:58 +0100

    Caclulation of net O3 change, updated sequence of number of carbon bonds

--- matlab/ANsCB_read_chem.m
+++ matlab/ANsCB_read_chem.m
@@ -35,6 +35,7 @@ for iANs = 1:length(ANs)
         mixrat{iANs,iNOx} = aggr_array./M*1.0e+9; % mixing ratio
     end
 end
+xend = 96;
 %% Plot mixing ratios timeseries
 spseqfac = {'O3' 'O1D' 'OH' 'NO' 'NO2' ...
     'HO2' 'H2O2' 'CO' 'HNO3' 'CH4' ...
@@ -54,11 +55,8 @@ spseqfac = {'O3' 'O1D' 'OH' 'NO' 'NO2' ...
 	'IC5H12' 'IPEAO' 'IPEBO' 'IPECO' 'IPEAO2' ...
 	'IPEBO2' 'IPECO2' 'IPEAOOH' 'IPEBOOH' 'IPECOOH' ...
 	'BUT2CHO' 'MIPK' 'IPEANO3' 'IPEBNO3' 'IPECNO3'};
-xend = 96;
-
 % Set the index of experiment
 iANs = 1;
-
 for iNOx = 1:length(NOx)
     C3H7O2 = mixrat{iANs,iNOx}(:,27)+mixrat{iANs,iNOx}(:,29);
     nC4H9O2 = mixrat{iANs,iNOx}(:,41)+mixrat{iANs,iNOx}(:,42);
@@ -244,39 +242,39 @@ export_fig(imgname);
 
 end
 %% Plot all in one figure
+outdir = common_outdir;
 nrows = 4;
 ncols = 7;
 fig=figure;
 for iNOx = 1:length(NOx)
-    subplot(nrows,ncols,1);  plot(mixrat{iANs,iNOx}(:,1),'LineWidth',2,'Color','b'); title('O_3','Fontsize',10);
-	subplot(nrows,ncols,2);  plot(mixrat{iANs,iNOx}(:,2),'LineWidth',2,'Color','b'); title('O(^1D)','Fontsize',10);
-	subplot(nrows,ncols,3);  plot(mixrat{iANs,iNOx}(:,3),'LineWidth',2,'Color','b'); title('OH','Fontsize',10);
-	subplot(nrows,ncols,4);  plot(mixrat{iANs,iNOx}(:,4),'LineWidth',2,'Color','b'); title('NO','Fontsize',10);
-	subplot(nrows,ncols,5);  plot(mixrat{iANs,iNOx}(:,5),'LineWidth',2,'Color','b'); title('NO_2','Fontsize',10);
-	subplot(nrows,ncols,6);  plot(mixrat{iANs,iNOx}(:,6),'LineWidth',2,'Color','b'); title('HO_2','Fontsize',10);
-	subplot(nrows,ncols,7);  plot(mixrat{iANs,iNOx}(:,8),'LineWidth',2,'Color','b'); title('CO','Fontsize',10);
-	subplot(nrows,ncols,8);  plot(mixrat{iANs,iNOx}(:,10),'LineWidth',2,'Color','b'); title('CH_4','Fontsize',10);
-	subplot(nrows,ncols,9);  plot(mixrat{iANs,iNOx}(:,16),'LineWidth',2,'Color','b'); title('C_2H_6','Fontsize',10);
-	subplot(nrows,ncols,10); plot(mixrat{iANs,iNOx}(:,25),'LineWidth',2,'Color','b'); title('C_3H_8','Fontsize',10);
-	subplot(nrows,ncols,11); plot(mixrat{iANs,iNOx}(:,38),'LineWidth',2,'Color','b'); title('n-C_4H_1_0','Fontsize',10);
-	subplot(nrows,ncols,12); plot(mixrat{iANs,iNOx}(:,50),'LineWidth',2,'Color','b'); title('i-C_4H_1_0','Fontsize',10);
-	subplot(nrows,ncols,13); plot(mixrat{iANs,iNOx}(:,60),'LineWidth',2,'Color','b'); title('n-C_5H_1_2','Fontsize',10);
-	subplot(nrows,ncols,14); plot(mixrat{iANs,iNOx}(:,76),'LineWidth',2,'Color','b'); title('i-C_5H_1_2','Fontsize',10);
-	subplot(nrows,ncols,15); plot(mixrat{iANs,iNOx}(:,13),'LineWidth',2,'Color','b'); title('CH_3O_2','Fontsize',10);
-	subplot(nrows,ncols,16); plot(mixrat{iANs,iNOx}(:,18),'LineWidth',2,'Color','b'); title('C_2H_5O_2','Fontsize',10);
-	subplot(nrows,ncols,17); plot(C3H7O2,'LineWidth',2,'Color','b'); title('\Sigma C_3H_7O_2','Fontsize',10);
-	subplot(nrows,ncols,18); plot(nC4H9O2,'LineWidth',2,'Color','b'); title('\Sigma n-C_4H_9O_2','Fontsize',10);
-	subplot(nrows,ncols,19); plot(iC4H9O2,'LineWidth',2,'Color','b'); title('\Sigma i-C_4H_9O_2','Fontsize',10);
-	subplot(nrows,ncols,20); plot(nC5H11O2,'LineWidth',2,'Color','b'); title('\Sigma n-C_5H_1_1O_2','Fontsize',10);
-	subplot(nrows,ncols,21); plot(iC5H11O2,'LineWidth',2,'Color','b'); title('\Sigma i-C_5H_1_1O_2','Fontsize',10);
-	subplot(nrows,ncols,22); plot(mixrat{iANs,iNOx}(:,15),'LineWidth',2,'Color','b'); title('CH_3ONO_2','Fontsize',10);
-	subplot(nrows,ncols,23); plot(mixrat{iANs,iNOx}(:,24),'LineWidth',2,'Color','b'); title('C_2H_5ONO_2','Fontsize',10);
-	subplot(nrows,ncols,24); plot(C3H7NO3,'LineWidth',2,'Color','b'); title('\Sigma C_3H_7ONO_2','Fontsize',10);
-	subplot(nrows,ncols,25); plot(nC4H9NO3,'LineWidth',2,'Color','b'); title('\Sigma n-C_4H_9ONO_2','Fontsize',10);
-	subplot(nrows,ncols,26); plot(iC4H9NO3,'LineWidth',2,'Color','b'); title('\Sigma i-C_4H_9ONO_2','Fontsize',10);
-	subplot(nrows,ncols,27); plot(nC5H11NO3,'LineWidth',2,'Color','b'); title('\Sigma n-C_5H_1_1ONO_2','Fontsize',10);
-	subplot(nrows,ncols,28); plot(iC5H11NO3,'LineWidth',2,'Color','b'); title('\Sigma i-C_5H_1_1ONO_2','Fontsize',10);
-
+    subplot(nrows,ncols,1);  plot(mixrat{iANs,iNOx}(:,1),'LineWidth',2,'Color','b'); title('O_3','Fontsize',7);
+	subplot(nrows,ncols,2);  plot(mixrat{iANs,iNOx}(:,2),'LineWidth',2,'Color','b'); title('O(^1D)','Fontsize',7);
+	subplot(nrows,ncols,3);  plot(mixrat{iANs,iNOx}(:,3),'LineWidth',2,'Color','b'); title('OH','Fontsize',7);
+	subplot(nrows,ncols,4);  plot(mixrat{iANs,iNOx}(:,4),'LineWidth',2,'Color','b'); title('NO','Fontsize',7);
+	subplot(nrows,ncols,5);  plot(mixrat{iANs,iNOx}(:,5),'LineWidth',2,'Color','b'); title('NO_2','Fontsize',7);
+	subplot(nrows,ncols,6);  plot(mixrat{iANs,iNOx}(:,6),'LineWidth',2,'Color','b'); title('HO_2','Fontsize',7);
+	subplot(nrows,ncols,7);  plot(mixrat{iANs,iNOx}(:,8),'LineWidth',2,'Color','b'); title('CO','Fontsize',7);
+	subplot(nrows,ncols,8);  plot(mixrat{iANs,iNOx}(:,10),'LineWidth',2,'Color','b'); title('CH_4','Fontsize',7);
+	subplot(nrows,ncols,9);  plot(mixrat{iANs,iNOx}(:,16),'LineWidth',2,'Color','b'); title('C_2H_6','Fontsize',7);
+	subplot(nrows,ncols,10); plot(mixrat{iANs,iNOx}(:,25),'LineWidth',2,'Color','b'); title('C_3H_8','Fontsize',7);
+	subplot(nrows,ncols,11); plot(mixrat{iANs,iNOx}(:,38),'LineWidth',2,'Color','b'); title('n-C_4H_1_0','Fontsize',7);
+	subplot(nrows,ncols,12); plot(mixrat{iANs,iNOx}(:,50),'LineWidth',2,'Color','b'); title('i-C_4H_1_0','Fontsize',7);
+	subplot(nrows,ncols,13); plot(mixrat{iANs,iNOx}(:,60),'LineWidth',2,'Color','b'); title('n-C_5H_1_2','Fontsize',7);
+	subplot(nrows,ncols,14); plot(mixrat{iANs,iNOx}(:,76),'LineWidth',2,'Color','b'); title('i-C_5H_1_2','Fontsize',7);
+	subplot(nrows,ncols,15); plot(mixrat{iANs,iNOx}(:,13),'LineWidth',2,'Color','b'); title('CH_3O_2','Fontsize',7);
+	subplot(nrows,ncols,16); plot(mixrat{iANs,iNOx}(:,18),'LineWidth',2,'Color','b'); title('C_2H_5O_2','Fontsize',7);
+	subplot(nrows,ncols,17); plot(C3H7O2,'LineWidth',2,'Color','b'); title('\Sigma C_3H_7O_2','Fontsize',7);
+	subplot(nrows,ncols,18); plot(nC4H9O2,'LineWidth',2,'Color','b'); title('\Sigma n-C_4H_9O_2','Fontsize',7);
+	subplot(nrows,ncols,19); plot(iC4H9O2,'LineWidth',2,'Color','b'); title('\Sigma i-C_4H_9O_2','Fontsize',7);
+	subplot(nrows,ncols,20); plot(nC5H11O2,'LineWidth',2,'Color','b'); title('\Sigma n-C_5H_1_1O_2','Fontsize',7);
+	subplot(nrows,ncols,21); plot(iC5H11O2,'LineWidth',2,'Color','b'); title('\Sigma i-C_5H_1_1O_2','Fontsize',7);
+	subplot(nrows,ncols,22); plot(mixrat{iANs,iNOx}(:,15),'LineWidth',2,'Color','b'); title('CH_3ONO_2','Fontsize',7);
+	subplot(nrows,ncols,23); plot(mixrat{iANs,iNOx}(:,24),'LineWidth',2,'Color','b'); title('C_2H_5ONO_2','Fontsize',7);
+	subplot(nrows,ncols,24); plot(C3H7NO3,'LineWidth',2,'Color','b'); title('\Sigma C_3H_7ONO_2','Fontsize',7);
+	subplot(nrows,ncols,25); plot(nC4H9NO3,'LineWidth',2,'Color','b'); title('\Sigma n-C_4H_9ONO_2','Fontsize',7);
+	subplot(nrows,ncols,26); plot(iC4H9NO3,'LineWidth',2,'Color','b'); title('\Sigma i-C_4H_9ONO_2','Fontsize',7);
+	subplot(nrows,ncols,27); plot(nC5H11NO3,'LineWidth',2,'Color','b'); title('\Sigma n-C_5H_1_1ONO_2','Fontsize',7);
+	subplot(nrows,ncols,28); plot(iC5H11NO3,'LineWidth',2,'Color','b'); title('\Sigma i-C_5H_1_1ONO_2','Fontsize',7);
  faxes = findobj(fig,'Type','Axes');
  xlimits = [0 xend];
  xx =0:(xend)/2:xend;
@@ -288,25 +286,85 @@ for iNOx = 1:length(NOx)
      xlim(faxes(i),xlimits);
      set(faxes(i),'XTick',xx,'XTickLabel',xxlab)
  end
-imgname28 = strcat(outdir,'/',part,'_',num2srt(iANs),'_',num2str(NOx),'.png');
+imgname28 = strcat(outdir,'/',part,'_',num2str(ANs(iANs)),'_',num2str(NOx(iNOx)),'_28.png');
 set(gcf,'visible','off')
 print(gcf,'-dpng','-r300',imgname28);
-
 end
+%% Net O3 loss/production vs NOx
+clc
+outdir = common_outdir;
+fig1 = figure; %set(gcf,'visible','off')
+% fig2 = figure; %set(fig2,'visible','off');
+% fig3 = figure;
+cvec =[0.25 0.25 0.25
+          0.25 0.25 0.75 
+          0.25 0.75 1.00 
+          1.00 0.50 0.00
+          0.25 0.75 0.25
+          1.00 0.00 0.25
+          0.75 0.25 0.25
+          1.00 0.75 0.25
+          0.25 1.00 0.00
+          0.50 0.25 0.75
+          0.50 0.75 0.75
+          0.75 0.25 0.25
+          0.25 1.00 0.25];
+
+for iANs = 1:length(ANs)
+    for iNOx = 1:length(NOx)
+        O3rate(iANs,iNOx,:) =  gradient(mixrat{iANs,iNOx}(:,1))./15.*60;
+        NOxabun(iANs,iNOx,:) =  (mixrat{iANs,iNOx}(:,38)+mixrat{iANs,iNOx}(:,50));
+%         figure(fig2)
+%         scatter(NOxabun(iANs,iNOx,:),O3rate(iANs,iNOx,:),2^6,cvec(iANs,:));
+%         hold on;
+%             figure(fig3)
+%             plot(mixrat{iANs,1}(:,1)); title(strcat('NOx = ',num2str(NOx(1))));
+%             hold on;
+    end
+end
+% Net O3 production in base experiment as a =f(NOx)
+for iNOx = 1:length(NOx)
+    O3netbase(iNOx) = mixrat{1,iNOx}(end,1)-mixrat{1,iNOx}(1,1); % final mixrat - initial mixrat
+end
+
+for iANs = 2:length(ANs)
+    for iNOx = 1:length(NOx)
+        O3net_other(iNOx) = mixrat{iANs,iNOx}(end,1)-mixrat{iANs,iNOx}(1,1);
+    end
+    O3netdiff(iANs,:) = O3netbase - O3net_other;
+    figure(fig1)
+    plot(NOx,O3netdiff(iANs,:),'Color',cvec(iANs,:),'LineWidth',2);
+    hold on;
+ end
+% figure; contourf(mean(O3rate(:,:,:),3)); colorbar
+imgnameO3NOxdiff = strcat(outdir,'/',part,'_O3NOxdiff.png');
+figure(fig1)
+title('Differences in net O_3 production/loss depending on RONO_2 chemistry');
+legend('base - CH_3ONO_2','base - C_2H_5ONO_2','base - \Sigma C_3H_7ONO_2',...
+'base - \Sigma n-C_4H_9ONO_2','base - \Sigma i-C_4H_9ONO_2','base - \Sigma n-C_5H_1_2ONO_2',...
+'base - \Sigma i-C_5H_1_2ONO_2','Location','east');
+xlabel('NOx, ppt');
+ylabel('Net O_3 change, ppb');
+print(gcf,'-dpng','-r300',imgnameO3NOxdiff);
 %% Calculate number of carbon bonds
 ncb = [0 0 0 0 0 ...
-    0 0 1 0 4 2 ...
-    3 3 3 3 7 ...
-    6 6 6 6 5 ...
-    4 4 6 10 9 ... 
-    9 9 9 9 9 ...
-    8 7 7 8 9 ...
-    9 13 12 12 12 ...
-    12 12 12 10 11 ...
-    11 12 12 16 15 ...
-    15 15 15 15 15 ...
-    15 15 15 14 14 ...
-    14 14 14 14];
+    0 0 1 0 4 ...
+	2 3 3 3 3 ...
+	7 6 6 6 6 ...
+	5 4 4 6 10 ...
+	9 9 9 9 9 ...
+	9 8 7 7 8 ...
+	9 9 13 12 12 ...
+	12 12 12 12 10 ...
+	11 11 12 12 13 ...
+	12 12 12 12 12 ...
+	12 12 12 12 16 ...
+	15 15 15 15 15 ...
+	15 15 15 15 14 ...
+	14 14 15 15 15 ...
+	16 15 15 15 15 ...
+	15 15 15 15 15 ...
+	14 14 15 15 15];
 ncb2D =  repmat(ncb,size(numden,1),1); % vertically stack a row vector
 Pbonds = numden.*ncb2D;
 RadPotential = sum(Pbonds(end,:),2);
